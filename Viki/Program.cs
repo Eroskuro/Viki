72f7afcdfbde9d057b24d2bc28358bfe69a30aa6
Eroskuro <40394796+Eroskuro@users.noreply.github.com>
2018-06-19 10:18:02 -0400

    INITIAL COMMIT

---
+++ Viki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Viki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
